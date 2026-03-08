56524a728e601bbb4faf8ec1eb0f84cd803e24fc
Pak Vera Alexandrovna <verapark.al@icloud.com>
2026-03-08 18:29:20 +0300

    Refactor PaymentMapper and Add WelcomeController

--- payment-service-app/src/main/java/com/iprody/payment/service/app/mapper/PaymentMapper.java
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/mapper/PaymentMapper.java
@@ -4,9 +4,14 @@ import com.iprody.payment.service.app.dto.CreatePaymentDto;
 import com.iprody.payment.service.app.dto.PaymentDto;
 import com.iprody.payment.service.app.persistence.entity.Payment;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PaymentMapper {
     PaymentDto toPaymentDto(Payment payment);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
     Payment fromCreatePaymentDto(CreatePaymentDto dto);
 }
