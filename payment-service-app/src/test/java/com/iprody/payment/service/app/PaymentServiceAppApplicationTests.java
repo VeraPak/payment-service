c395e22c4add9839553fa42e785620d94ffeb683
Pak Vera Alexandrovna <verapark.al@icloud.com>
2026-03-19 21:43:12 +0300

    Add integration tests

--- payment-service-app/src/test/java/com/iprody/payment/service/app/PaymentServiceAppApplicationTests.java
+++ payment-service-app/src/test/java/com/iprody/payment/service/app/PaymentServiceAppApplicationTests.java
@@ -2,11 +2,14 @@ package com.iprody.payment.service.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class PaymentServiceAppApplicationTests {
+
+    @MockitoBean
+    private JwtDecoder jwtDecoder;
 
     @Test
     void contextLoads() {
